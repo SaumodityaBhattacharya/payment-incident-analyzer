946ecd72fa92bb0773bf259a2f8f2601cf7132a8
SaumodityaB <bhattacharyasaumodityo@gmail.com>
2026-08-26 20:28:35 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/saumoditya/package_incident_analyzer/PackageIncidentAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.saumoditya.package_incident_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageIncidentAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageIncidentAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saumoditya/package_incident_analyzer/PackageIncidentAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saumoditya.package_incident_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageIncidentAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
